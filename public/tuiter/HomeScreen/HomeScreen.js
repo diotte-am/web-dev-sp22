9d97e781151e7652b26d07d961d79e36e5562c29
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-02-27 07:00:21 -0500

    a5 challenge started

---
+++ public/tuiter/HomeScreen/HomeScreen.js
@@ -0,0 +1,23 @@
+
+import NavigationSidebar from "../NavigationSidebar";
+import ExploreComponent from "../ExploreScreen/ExploreComponent.js";
+import WhoToFollowList from "../WhoToFollowList";
+import PostSummaryList from "../PostSummaryList";
+
+(function ($) {
+    $('#wd-home').append(`
+        <div class="row mt-2">
+        <div class="col-2 col-md-2 col-lg-2 col-xl-2 p-0">
+            ${NavigationSidebar()}
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-10 col-sm-10 col-10 p-0 ps-2 pe-2">
+            ${ExploreComponent()};
+        </div>
+            
+        <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 pe-lg-2">
+            ${PostSummaryList()}
+        </div>
+        </div>
+    `);
+})($);
+
