c57cb0d88bf830fc46e31c1efc4db5794adc8866
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-03-06 16:29:15 -0500

    a6 lab tuiter component added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './vendors/css/tuiter-bootstrap.css';
 import './vendors/Font-Awesome-5.x/css/all.css';
 import './vendors/css/explore.css';
 import HelloWorld from "./components/HelloWorld";
+import Labs from "./components/Labs";
+import Tuiter from "./components/Tuiter";
+import React from "react";
+
+
 
 function App() {
   return (
       <div className="container">
         <HelloWorld/>
+          <Labs/>
+          <Tuiter/>
       </div>
 
   );
