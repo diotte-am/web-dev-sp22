c84c2749fa8f0a0a4f66fb55e7a8fea6778d5878
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-04-03 13:35:29 -0400

    a8 post content implemented

--- src/components/Tuiter/HomeScreen/WhatsHappening.js
+++ src/components/Tuiter/HomeScreen/WhatsHappening.js
@@ -1,39 +1,31 @@
 import React, {useState} from "react";
 import {useDispatch} from "react-redux";
-
+import {createTuit}
+    from "../actions/tuits-actions";
 
 
 const WhatsHappening = () => {
-    let [whatsHappening, setWhatsHappening]
-        = useState('');
+    const [newTuit, setNewTuit] =
+        useState({tuit: 'New tuit'});
     const dispatch = useDispatch();
-    const tuitClickHandler = () => {
-        dispatch({type: 'create-tuit',
-            tuit: whatsHappening
-        });
-    }
+
     return (
         <>
         <div className="flex-container pt-1 col-12 row">
             <div className="col-2">
-
-                <img alt="profilepic" src="/tuiter/image/amare.png" className="wd-rnd-icon-50"/>
-            </div>
-            <div className="col-10">
-              <textarea className="form-control text-body wd-grey-border" rows="2" placeholder="What's Happening" style={{backgroundColor: "#070807", resize: "none"}} value={whatsHappening}
-                        onChange={(event) =>
-                            setWhatsHappening(event.target.value)}>
-              </textarea>
-            </div>
-        </div>
-        <div className="flex-container row pb-2">
-            <div className="col-9">
-            </div>
-                <button className="btn btn-primary col-2" style={{borderRadius: "25px", border: "none", fontWeight: "bold"}}  onClick={tuitClickHandler} >
+                <button onClick={() =>
+                    createTuit(dispatch, newTuit)}
+                        className="btn btn-primary float-end">
                     Tuit
                 </button>
-
+                <textarea className="form-control w-75"
+                          onChange={(e) =>
+                              setNewTuit({...newTuit,
+                                  tuit: e.target.value})}></textarea>
+                <img alt="profilepic" src="/tuiter/image/amare.png" className="wd-rnd-icon-50"/>
+            </div>
         </div>
+
         </>
     );
 }
--- src/components/Tuiter/Reducers/TuitsReducer.js
+++ src/components/Tuiter/Reducers/TuitsReducer.js
@@ -1,8 +1,29 @@
-import {DELETE_TUIT, FIND_ALL_TUITS}
-    from "../actions/tuits-actions";
+import {
+    CREATE_TUIT,
+    DELETE_TUIT,
+    FIND_ALL_TUITS
+} from "../actions/tuits-actions";
 
 const tuitsReducer = (state = [], action) => {
     switch (action.type) {
+        case CREATE_TUIT:
+            const newTuit = {
+                'tuit': action.newTuit.action,
+                '_id': (new Date()).getTime() + '',
+                'postedBy': {
+                    'username': "ReactJS"
+                },
+                'stats': {
+                    'retuits': '111',
+                    'likes': '222',
+                    'replies': '333'
+                }
+            }
+            return [
+                newTuit,
+                ...state
+
+            ];
         case DELETE_TUIT:
             return state.filter(
                 tuit => tuit._id !== action.tuit._id);
