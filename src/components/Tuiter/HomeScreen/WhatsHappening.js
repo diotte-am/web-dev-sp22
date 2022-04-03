ac5c6d2c2865d14d10a8a17677e9f27e7c4514f0
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-04-03 13:41:22 -0400

    a8 fixed what's happening formatting

--- src/components/Tuiter/HomeScreen/WhatsHappening.js
+++ src/components/Tuiter/HomeScreen/WhatsHappening.js
@@ -13,17 +13,24 @@ const WhatsHappening = () => {
         <>
         <div className="flex-container pt-1 col-12 row">
             <div className="col-2">
-                <button onClick={() =>
-                    createTuit(dispatch, newTuit)}
-                        className="btn btn-primary float-end">
-                    Tuit
-                </button>
-                <textarea className="form-control w-75"
-                          onChange={(e) =>
-                              setNewTuit({...newTuit,
-                                  tuit: e.target.value})}></textarea>
                 <img alt="profilepic" src="/tuiter/image/amare.png" className="wd-rnd-icon-50"/>
             </div>
+            <div className="col-10">
+                 <textarea className="form-control text-body wd-grey-border" rows="2" placeholder="What's Happening" style={{backgroundColor: "#070807", resize: "none"}}
+                           onChange={(e) =>
+                               setNewTuit({...newTuit,
+                                   tuit: e.target.value})}></textarea>
+            </div>
+            <div className="flex-container row pb-2">
+                <div className="col-10">
+                </div>
+                <button onClick={() =>
+                    createTuit(dispatch, newTuit)}
+                        className="btn btn-primary col-2" style={{borderRadius: "25px", border: "none", fontWeight: "bold"}}>
+                    Tuit
+                </button>
+            </div>
+
         </div>
 
         </>
