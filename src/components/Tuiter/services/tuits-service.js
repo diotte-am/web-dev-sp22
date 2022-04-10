c3754e1386fd4caae036998ab636189a43a2fe62
diotte-am <83765247+diotte-am@users.noreply.github.com>
2022-04-10 14:12:11 -0400

    a9 api added to netlify

--- src/components/Tuiter/services/tuits-service.js
+++ src/components/Tuiter/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE_9;
 console.log("apibase " + API_BASE);
 const TUITS_API = `${API_BASE}/tuits`;
 
